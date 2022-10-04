1e026764824d45d554336df4e03ce77688934935
nikesh8 <nikesh@logicwind.com>
2022-10-04 11:55:08 +0530

    first commit

---
+++ MyCocoaPodDemo/Classes/Logger.swift
@@ -0,0 +1,20 @@
+//
+//  Logger.swift
+//  MyCocoaPodDemo
+//
+//  Created by lw-66 on 04/10/22.
+//
+
+import Foundation
+
+public class Logger {
+    public init(){}
+    
+    public func printLog() {
+        print("Hello world")
+    }
+    
+    func privateMethod() {
+        print("private")
+    }
+}
